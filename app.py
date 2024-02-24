7015f4dd7ece6f419a0c6eb0b57f26e5eb3ba7b1
username <chauhankay18@gmail.com>
2024-02-24 21:33:28 +0530

    Created initial UI layout

--- app.py
+++ app.py
@@ -3,25 +3,40 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+screen = pygame.display.set_mode((800, 800))
 
+# text written above the Pygame window
+pygame.display.set_caption("Connect4")
+
+# load image as icon
+icon = pygame.image.load("/home/kshitijc/Downloads/C.png")
+
+# set image as icon
+pygame.display.set_icon(icon)
+
+# function to draw circles
+def draw_circles(num_circles, num_lines, surface, color, start_x, start_y, radius, space):
+    for line in range(num_lines):
+        center_x = start_x
+        center_y = start_y + (2 * radius + space) * line
+        for i in range(num_circles):
+            pygame.draw.circle(surface, color, (center_x, center_y), radius)
+            center_x += radius * 2 + space
+        
+
+running = True
 while running:
-    # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    # background color
+    screen.fill("black")
 
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+    draw_circles(10, 10, screen, "silver", 100, 100, 30, 6)
+   
+    # flip() the display to put your work on screenreen
     pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
